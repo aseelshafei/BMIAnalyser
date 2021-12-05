5484b81e786139ba7d5f55937066b9ea47254eb3
aseel <shaaseel812@gmail.com>
2021-12-05 23:56:44 +0300

    Hello word

---
+++ app/src/main/java/com/example/bmianalyser/Registration.java
@@ -0,0 +1,14 @@
+package com.example.bmianalyser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
