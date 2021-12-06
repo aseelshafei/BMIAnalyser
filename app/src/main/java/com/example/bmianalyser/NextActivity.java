71dcc915efa4efa4432f144f319625bd6dea082d
aseel <shaaseel812@gmail.com>
2021-12-06 15:53:52 +0300

    Hello world

--- app/src/main/java/com/example/bmianalyser/NextActivity.java
+++ app/src/main/java/com/example/bmianalyser/NextActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class NextActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
     }
